7a82cabc88d397b8e137947bb9cfe4f04bb11c04
supersorbet <artificialperiod@gmail.com>
2024-10-05 21:18:30 -0400

    update

--- generate.js
+++ generate.js
@@ -18,18 +18,26 @@ function hash(tokenId, salt) {
     );
 }
 
+function findVanitySalt(factoryAddress, initCodeHash, tokenId, prefix) {
+    let salt, address;
+    do {
+        salt = ethers.utils.randomBytes(32);
+        address = computeCreate3Address(factoryAddress, salt, initCodeHash);
+    } while (!address.toLowerCase().startsWith(prefix.toLowerCase()));
+    return { salt, address };
+}
+
 async function main() {
     const factoryAddress = "0xDCB5Fd9685Ef0cF8b960775361e34Ca35c562D39";
     const initCode = "0x60288060093d393df36001600581360334348434363434376d01e4a82b33373de1334e7d8f48795af49247f034521b34f3";
     const initCodeHash = ethers.utils.keccak256(initCode);
-    const salts = Array.from({ length: 50 }, (_, i) => ({
-        tokenId: i,
-        salt: ethers.utils.randomBytes(32)
-    }));
+    const desiredPrefix = "0xb453d";
 
-    const addresses = salts.map(({ tokenId, salt }) => 
-        computeCreate3Address(factoryAddress, salt, initCodeHash)
-    );
+    const salts = [];
+    for (let i = 0; i < 5; i++) {
+        const { salt, address } = findVanitySalt(factoryAddress, initCodeHash, i, desiredPrefix);
+        salts.push({ tokenId: i, salt, address });
+    }
 
     const leaves = salts.map(({ tokenId, salt }) => hash(tokenId, salt));
     const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
@@ -37,13 +45,13 @@ async function main() {
 
     const result = {
         merkleRoot,
-        tokens: salts.map(({ tokenId, salt }, index) => {
+        tokens: salts.map(({ tokenId, salt, address }) => {
             const leaf = hash(tokenId, salt);
             const proof = tree.getHexProof(leaf);
             return {
                 tokenId,
                 salt: `0x${salt.toString('hex')}`,
-                address: addresses[index],
+                address,
                 proof
             };
         })
